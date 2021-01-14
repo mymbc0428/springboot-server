0d9ded449ded72919b0338f23bde738d11888225
mymbc0428 <mymbc1@naver.com>
2021-01-14 23:54:30 +0900

    등록, 수정 조회를 위한 기초 작업  - Repository, Service, Domain 추

---
+++ src/main/java/com/server/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.server.springboot.web;
+
+import com.server.springboot.service.posts.PostsService;
+import com.server.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto)
+    {
+        return postsService.save(requestDto);
+    }
+}
